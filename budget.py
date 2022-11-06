2166b52bde02f93da80c5073e3175753c157d454
SOME1HING <yashprakash2005@gmail.com>
2022-11-06 20:26:15 +0530

    finalizing

--- budget.py
+++ budget.py
@@ -2,43 +2,32 @@ from itertools import zip_longest
 from collections import OrderedDict
 
 
-def roundup(x):
-    x = round(x) / 10
-    x = round(x + 0.5)
-    return x * 10
-
-
 class Category:
     def __init__(self, name):
         self.name = name
-        self.ledger = list()
+        self.total = 0.00
+        self.ledger = []
 
-    def __str__(self):
-        title = f"{self.name:*^30}\n"
-        items = ""
-        total = 0
+    def __repr__(self):
+        response = f"{self.name:*^30}\n"
         for item in self.ledger:
-            items += (
-                f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + "\n"
-            )
-            total += item["amount"]
-        output = title + items + "Total: " + str(total)
-        return output
+            response += f'{item["description"][0:23]:23}{item["amount"]:>7.2f}\n'
+        response += f"Total: {self.total}"
+        return response
 
     def deposit(self, amount, description=""):
+        self.total += amount
         self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description=""):
         if self.check_funds(amount):
+            self.total -= amount
             self.ledger.append({"amount": -amount, "description": description})
             return True
         return False
 
     def get_balance(self):
-        total_cash = 0
-        for item in self.ledger:
-            total_cash += item["amount"]
-        return total_cash
+        return self.total
 
     def transfer(self, amount, category):
         if self.check_funds(amount):
@@ -49,33 +38,18 @@ class Category:
         return False
 
     def check_funds(self, amount):
-        if self.get_balance() >= amount:
+        if self.total >= amount:
             return True
         return False
 
 
-def verticalPrint(astring):
-    wordList = astring.split(" ")
-    wordAmount = len(wordList)
-
-    maxLen = 0
-    for i in range(wordAmount):
-        length = len(wordList[i])
-        if length >= maxLen:
-            maxLen = length
-
-    ### makes all words the same length to avoid range errors ###
-    for i in range(wordAmount):
-        if len(wordList[i]) < maxLen:
-            wordList[i] = wordList[i] + (" ") * (maxLen - len(wordList[i]))
-
-    for i in range(wordAmount):
-        for j in range(maxLen):
-            print(wordList[i][j])
+def roundup(x):
+    x = round((round(x) / 10) + 0.5) * 10
+    return x
 
 
 def create_spend_chart(categories):
-    s = "Percentage spent by category\n"
+    text = "Percentage spent by category\n"
 
     total = 0
     cats = OrderedDict()
@@ -92,22 +66,22 @@ def create_spend_chart(categories):
         percent = (val / abs(total)) * 100
         cats[key] = roundup(percent)
 
-    for n in range(100, -10, -10):
-        s += f"{n}|".rjust(4)
+    for i in range(100, -10, -10):
+        text += f"{i}|".rjust(4)
         for val in cats.values():
-            if val > n:
-                s += " o "
+            if val > i:
+                text += " o "
             else:
-                s += "   "
-        s += " \n"
+                text += "   "
+        text += " \n"
 
     length = len(cats.values())
-    s += "    " + "-" * (length * 3 + 1) + "\n"
+    text += "    " + "-" * (length * 3 + 1) + "\n"
 
     temp_string = ""
     for key in cats.keys():
         temp_string += key.lower().capitalize() + " "
     for x in zip_longest(*temp_string.split(), fillvalue=" "):
-        s += "     " + "  ".join(x) + "  \n"
-    s = s.rstrip()
-    return s + "  "
+        text += "     " + "  ".join(x) + "  \n"
+    text = text.rstrip() + "  "
+    return text
