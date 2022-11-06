2ddf3988e003894b5d24a01d9f73cdabcf7fa211
SOME1HING <yashprakash2005@gmail.com>
2022-11-06 19:29:42 +0530

    Update budget.py

--- budget.py
+++ budget.py
@@ -1,10 +1,5 @@
 from itertools import zip_longest
 from collections import OrderedDict
-import math
-
-0.7
-7.0
-2.2
 
 
 def roundup(x):
@@ -112,25 +107,7 @@ def create_spend_chart(categories):
     temp_string = ""
     for key in cats.keys():
         temp_string += key.lower().capitalize() + " "
-    for x in zip_longest(*temp_string.split(" "), fillvalue=" "):
+    for x in zip_longest(*temp_string.split(), fillvalue=" "):
         s += "     " + "  ".join(x) + "  \n"
-
-    return s
-
-
-food = Category("Food")
-entertainment = Category("Entertainment")
-business = Category("Business")
-
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-hmm = create_spend_chart([business, food, entertainment])
-
-lul = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
-
-print(bool(hmm == lul))
-print(hmm)
+    s = s.rstrip()
+    return s + "  "
